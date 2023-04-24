b18b26725b5708cc96305ad0e551bedfa8c4be66
maxefimov <efimov.top.academy@gmail.com>
2023-04-24 20:51:46 +0300

    Добавьте файлы проекта.

---
+++ WpfBindingDataContextApp/Employee.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfBindingDataContextApp
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        string? name;
+        int age;
+
+        public string? Name 
+        {
+            set
+            {
+                if(name != value)
+                {
+                    name = value;
+                    NotifyPropertyChanged(nameof(Name));
+                }
+            }
+            get => name;
+        }
+        public int Age 
+        { 
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    NotifyPropertyChanged(nameof(Age));
+                }
+            }
+            get => age;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    //public class Employee
+    //{
+
+    //    public string? Name { set; get; }
+    //    public int Age { set; get; }
+
+    //}
+
+    //public class Employee : DependencyObject
+    //{
+    //    public static DependencyProperty NameProperty;
+    //    public static DependencyProperty AgeProperty;
+    //    static Employee()
+    //    {
+    //        NameProperty = DependencyProperty.Register(
+    //            "Name",
+    //            typeof(string),
+    //            typeof(Employee)
+    //            );
+    //        AgeProperty = DependencyProperty.Register(
+    //            "Age",
+    //            typeof(int),
+    //            typeof(Employee)
+    //            );
+    //    }
+
+    //    public string? Name
+    //    {
+    //        get { return (string?)GetValue(NameProperty); }
+    //        set { SetValue(NameProperty, value); }
+    //    }
+    //    //public Company? Company { get; set; }
+    //    public int Age
+    //    {
+    //        get { return (int)GetValue(AgeProperty); }
+    //        set { SetValue(AgeProperty, value); }
+    //    }
+    //}
+
+    public class Company
+    {
+        public string? Title { get; set; }
+    }
+}
---
+++ WpfBindingDataContextApp/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingDataContextApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Employee employee;
+
+        //public static DependencyProperty EmployeeProperty;
+
+        //public Employee Employee
+        //{
+        //    set
+        //    {
+        //        employee = value;
+        //        //SetValue(EmployeeProperty, value);
+        //    }
+        //    get 
+        //    { 
+        //        return employee;
+        //        //return (Employee)GetValue(EmployeeProperty);
+        //    }
+        //}
+
+        //static MainWindow()
+        //{
+        //    EmployeeProperty = DependencyProperty.Register(
+        //        "Employee",
+        //        typeof(Employee),
+        //        typeof(MainWindow)
+        //        );
+        //}
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            employee = new()
+            {
+                Name = "Joe",
+                //Company = new() { Title = "Yandex" },
+                Age = 35
+            };
+
+            this.DataContext = employee;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            employee.Name = "Sam";
+            employee.Age = 44;
+            //Employee employee = (Employee)this.Resources["employee"];
+            //MessageBox.Show($"{employee.Name} {employee.Age}");
+            //((Employee)this.Resources["employee"]).Name = "Sam";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{employee.Name} {employee.Age}");
+        }
+    }
+}
